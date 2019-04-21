22f66b2b6943fa3587f2bd7a99c7679fa48d35f8
Natasha Chernis <natashachernis@gmail.com>
2019-04-21 00:01:42 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -26,15 +26,15 @@ class MusicLibraryController
         when "list songs"
           list_songs
         when "list artists"
-          self.list_artists
+          list_artists
         when "list genres"
-          self.list_genres
+          list_genres
         when "list artist"
-          self.list_songs_by_artist
+          list_songs_by_artist
         when "list genre"
-          self.list_songs_by_genre
+          list_songs_by_genre
         when "play song"
-          self.play_song
+          play_song
       end
     end
 
